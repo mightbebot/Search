00d059a147aa90d605cb6b9b98fdd89d1780b75d
mightbebot <87195752+mightbebot@users.noreply.github.com>
2021-09-22 10:19:47 +1000

    Update actions.py

--- game_engine/actions.py
+++ game_engine/actions.py
@@ -1,8 +1,7 @@
 # actions.py
 # --------------
-# COMP3620/6320 Artificial Intelligence
-# The Australian National University
-# For full attributions, see attributions.txt on Wattle at the end of the course
+# Artificial Intelligence
+# mighty-botter
 
 """ This file defines the classes Directions and Actions.
 
