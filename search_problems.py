0812898ce9777993f365d041a55a5c822bd8f9d4
mightbebot <87195752+mightbebot@users.noreply.github.com>
2021-09-22 10:46:35 +1000

    Update search_problems.py

--- search_problems.py
+++ search_problems.py
@@ -1,8 +1,7 @@
 # search_problems.py
 # --------------
-# COMP3620/6320 Artificial Intelligence
-# The Australian National University
-# For full attributions, see attributions.txt on Wattle at the end of the course
+# Artificial Intelligence
+# mighty-botter
 
 """ This file contains a number of different search problems which you will
     be working with throughout the assignment.
