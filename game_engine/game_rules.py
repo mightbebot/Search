0b778fcc6759eb12621d7a3f10286d10f5f84cdf
mightbebot <87195752+mightbebot@users.noreply.github.com>
2021-09-22 10:21:21 +1000

    Update game_rules.py

--- game_engine/game_rules.py
+++ game_engine/game_rules.py
@@ -1,8 +1,7 @@
 # game_rules.py
 # --------------
-# COMP3620/6320 Artificial Intelligence
-# The Australian National University
-# For full attributions, see attributions.txt on Wattle at the end of the course
+# Artificial Intelligence
+# mighty-botter
 
 """ This file defines the rules of the game being played and the rules for both
     the red bird and the black bird.
